94ea59f457c3b70e0e3d917e949a82873269f84b
richshox050@gmail.com <richshox050@gmail.com>
2025-03-27 21:00:18 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,10 @@
 import asyncio
 from playwright.async_api import async_playwright
+from telegram import Bot
+
+TOKEN = "8128482653:AAHPBfPpinWcif2IBiSfn_3VILBcJ3nBkdw"
+CHAT_ID = "1668084744"
+bot = Bot(token=TOKEN)
 
 async def run():
     async with async_playwright() as p:
@@ -7,8 +12,8 @@ async def run():
         page = await browser.new_page()
         await page.goto("https://www.taobao.com")
         await page.screenshot(path="screenshot.png")
-        print("✅ Sahifa ochildi va screenshot olindi.")
         await browser.close()
+        bot.send_message(chat_id=CHAT_ID, text="✅ Taobao sahifasi ochildi va screenshot olindi!")
 
 if __name__ == "__main__":
     asyncio.run(run())
